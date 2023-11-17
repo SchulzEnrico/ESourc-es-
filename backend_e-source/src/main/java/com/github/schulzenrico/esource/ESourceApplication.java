9618dca11222de3ad775546767793010fcafb33b
Enrico Schulz <107748954+es-you@users.noreply.github.com>
2023-11-17 15:15:34 +0100

    Add backend module

---
+++ backend_e-source/src/main/java/com/github/schulzenrico/esource/ESourceApplication.java
@@ -0,0 +1,13 @@
+package com.github.schulzenrico.esource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ESourceApplication.class, args);
+	}
+
+}
