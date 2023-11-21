c04fbc83d30e5d1784d3dc96efe061441ec43e38
Enrico Schulz <107748954+es-you@users.noreply.github.com>
2023-11-21 13:27:47 +0100

    Fix  * the font-family   -unexpected missing generic font family
    Add
     * a tag to the Dashboard-icon in header

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -7,7 +7,9 @@ function Header() {
     return (
         <header>
                 <div id={"heading"}>
-                    <img alt={"Dashboard Icon"} id={"dash-icon-header"} className={"logo"} src="../src/assets/dash-red-192x192.png" />
+                    <a title={"Daschboard"} href={""} >
+                        <img alt={"Dashboard Icon"} id={"dash-icon-header"} className={"logo"} src="../src/assets/dash-red-192x192.png" />
+                    </a>
                     <h1>Sourc(es)</h1>
                 </div>
                 <div id={"creativity-boost"}>
