c8d071adaf2178014a05ea08db56a7f9f9422b9a
Enrico Schulz <107748954+es-you@users.noreply.github.com>
2023-12-09 19:17:27 +0100

    (CREATE) - Main grid sketch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
 import './App.css'
 import Header from "./components/header/Header.tsx";
 import Footer from "./components/footer/Footer.tsx";
+import Desk from "./components/main/Desk.tsx";
 
 
 function App() {
 
   return (
-    <>
+    <div className="app">
         <Header />
+        <Desk />
         <Footer />
-    </>
+    </div>
   )
 }
 
