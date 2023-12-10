d693dc2c1b8d05d2abea3e6779354bedbc4aba4a
Enrico Schulz <107748954+es-you@users.noreply.github.com>
2023-12-10 15:17:15 +0100

    (REBUILD) - the Desk component for using the panels
    (ADD)
    - a search on Google function in the footer
    
    (FIX)
    - some Styles

--- frontend/src/components/main/Desk.tsx
+++ frontend/src/components/main/Desk.tsx
@@ -1,72 +1,38 @@
 import './DeskGrid.css';
 import './DeskStyles.css'
 import { Container, Row, Col } from 'react-bootstrap';
+import Panel from "./Panel.tsx";
 
 function Desk() {
     return (
         <Container className="esources-desk">
             <Row className="garage">
                 <Col className="ins_pro panel shadow--raised">
-                    <Container className="panel-items-container">
-                        <div className="iframe-bounding shadow--sunken">
-                            <iframe></iframe>
-                        </div>
-                        <div className="panel-controls">sunken/controls</div>
-                    </Container>
+                    <Panel className="ins_pro" />
                 </Col>
                 <Col className="snip_gen panel shadow--raised">
-                    <Container className="panel-items-container">
-                        <div className="iframe-bounding shadow--sunken">
-                            <iframe></iframe>
-                        </div>
-                        <div className="panel-controls">sunken/controls</div>
-                    </Container>
+                    <Panel className="snip_gen" />
                 </Col>
             </Row>
             <Row className="workstation">
                 <Col className="development panel shadow--raised">
-                    <Container className="panel-items-container">
-                        <div className="iframe-bounding shadow--sunken">
-                            <iframe></iframe>
-                        </div>
-                        <div className="panel-controls">sunken/controls</div>
-                    </Container>
+                    <Panel className="development" />
                 </Col>
             </Row>
             <Row className="library">
                 <Col className="know_guide panel shadow--raised">
-                    <Container className="panel-items-container">
-                        <div className="iframe-bounding shadow--sunken">
-                            <iframe></iframe>
-                        </div>
-                        <div className="panel-controls">sunken/controls</div>
-                    </Container>
+                    <Panel className="know_guide" />
                 </Col>
                 <Col className="lip_doc panel shadow--raised">
-                    <Container className="panel-items-container">
-                        <div className="iframe-bounding shadow--sunken">
-                            <iframe></iframe>
-                        </div>
-                        <div className="panel-controls">sunken/controls</div>
-                    </Container>
+                    <Panel className="lip_doc" />
                 </Col>
             </Row>
             <Row className="managements">
                 <Col className="project panel shadow--raised">
-                    <Container className="panel-items-container">
-                        <div className="iframe-bounding shadow--sunken">
-                            <iframe></iframe>
-                        </div>
-                        <div className="panel-controls">sunken/controls</div>
-                    </Container>
+                    <Panel className="project" />
                 </Col>
                 <Col className="personal panel shadow--raised">
-                    <Container className="panel-items-container">
-                        <div className="iframe-bounding shadow--sunken">
-                            <iframe></iframe>
-                        </div>
-                        <div className="panel-controls">sunken/controls</div>
-                    </Container>
+                    <Panel className="personal" />
                 </Col>
             </Row>
         </Container>
