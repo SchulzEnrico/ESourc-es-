2b01dfa542fcfcf6580d826c0ee19fe20a72125e
Enrico Schulz <107748954+es-you@users.noreply.github.com>
2024-01-27 10:54:24 +0100

    (ADD) - A Close button for the Navigation Modal

--- frontend/src/components/nav/Navigation.tsx
+++ frontend/src/components/nav/Navigation.tsx
@@ -14,6 +14,7 @@ import EditBookmark from "./EditBookmark";
 import {NavigationProps, BookmarkDTO} from "../types/types.ts";
 import {TiPlus} from "react-icons/ti";
 import {MdOutlineSettings} from "react-icons/md";
+import {VscEyeClosed} from "react-icons/vsc";
 
 const Navigation: React.FC<NavigationProps> = ({ onLinkClick, panelName, isExternal, showModal, closeModal }) => {
     const tempBookmark = useRef<BookmarkDTO | null>(null);
@@ -186,6 +187,9 @@ const Navigation: React.FC<NavigationProps> = ({ onLinkClick, panelName, isExter
 
         return (
             <ButtonGroup className={isMobile ? "nav-group-mobile" : "nav-group"}>
+                <Button id={"close-navigation-btn"} onClick={closeModal}>
+                    <VscEyeClosed id={"close-navigation-icon"} />
+                </Button>
                 {!isMobile &&
                     uniqueCategories.map((category) => (
                         <DropdownButton
